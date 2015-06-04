d4a43b143475b83aa0033ffd4ecc53cf180e90f0
Mark R. Boyd <boydmr@ldschurch.org>
2015-06-04 16:21:16 -0600

    checkstyle fixes

--- src/main/java/com/sun/identity/authentication/modules/radius/client/AttributeSet.java
+++ src/main/java/com/sun/identity/authentication/modules/radius/client/AttributeSet.java
@@ -1,33 +1,18 @@
-/**
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
- * Copyright (c) 2007 Sun Microsystems Inc. All Rights Reserved
- *
- * The contents of this file are subject to the terms
- * of the Common Development and Distribution License
- * (the License). You may not use this file except in
- * compliance with the License.
- *
- * You can obtain a copy of the License at
- * https://opensso.dev.java.net/public/CDDLv1.0.html or
- * opensso/legal/CDDLv1.0.txt
- * See the License for the specific language governing
- * permission and limitations under the License.
- *
- * When distributing Covered Code, include this CDDL
- * Header Notice in each file and include the License file
- * at opensso/legal/CDDLv1.0.txt.
- * If applicable, add the following below the CDDL Header,
- * with the fields enclosed by brackets [] replaced by
- * your own identifying information:
- * "Portions Copyrighted [year] [name of copyright owner]"
- *
- * $Id: AttributeSet.java,v 1.2 2008/06/25 05:42:00 qcheng Exp $
- *
- */
-
 /*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
+ *
  * Portions Copyrighted [2011] [ForgeRock AS]
+ * Copyright 2015 LDS
  */
 package com.sun.identity.authentication.modules.radius.client;
 
