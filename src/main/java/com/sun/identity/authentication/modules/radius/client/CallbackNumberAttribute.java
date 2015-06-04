3d4389f223a9f6a14f33d989d7205a85442573cf
Mark R. Boyd <boydmr@ldschurch.org>
2015-06-04 17:00:21 -0600

    checkstyle fixes

--- src/main/java/com/sun/identity/authentication/modules/radius/client/CallbackNumberAttribute.java
+++ src/main/java/com/sun/identity/authentication/modules/radius/client/CallbackNumberAttribute.java
@@ -1,58 +1,71 @@
-/**
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
- * Copyright (c) 2007 Sun Microsystems Inc. All Rights Reserved
- *
- * The contents of this file are subject to the terms
- * of the Common Development and Distribution License
- * (the License). You may not use this file except in
- * compliance with the License.
- *
- * You can obtain a copy of the License at
- * https://opensso.dev.java.net/public/CDDLv1.0.html or
- * opensso/legal/CDDLv1.0.txt
- * See the License for the specific language governing
- * permission and limitations under the License.
- *
- * When distributing Covered Code, include this CDDL
- * Header Notice in each file and include the License file
- * at opensso/legal/CDDLv1.0.txt.
- * If applicable, add the following below the CDDL Header,
- * with the fields enclosed by brackets [] replaced by
- * your own identifying information:
- * "Portions Copyrighted [year] [name of copyright owner]"
- *
- * $Id: CallbackNumberAttribute.java,v 1.2 2008/06/25 05:42:00 qcheng Exp $
- *
- */
-
 /*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
+ *
  * Portions Copyrighted [2011] [ForgeRock AS]
+ * Portions Copyrighted [2015] [Intellectual Reserve, Inc (IRI)]
  */
 package com.sun.identity.authentication.modules.radius.client;
 
-import java.io.*;
+/**
+ * Class representing the structure of the Callback-Number attribute as specified in section 5.19 of RFC 2865 and
+ * able to be instantiated from the on-the-wire bytes.
+ */
+public class CallbackNumberAttribute extends Attribute {
+    /**
+     * The on-the-wire byte representation of the attribute.
+     */
+    private byte[] octets = null;
 
-public class CallbackNumberAttribute extends Attribute
-{
-	private byte _value[] = null;
-	private String _str = null;
+    /**
+     * The callback number value.
+     */
+    private String str = null;
 
-	public CallbackNumberAttribute(byte value[])
-	{
-		super();
-		_t = CALLBACK_NUMBER;
-		_str = new String(value, 2, value.length - 2);
-		_value = value;
-	}
+    /**
+     * Constructs a new instance from the on-the-wire bytes for this attribute.
+     *
+     * @param octets the on-the-wire bytes from which to construct this instance
+     */
+    public CallbackNumberAttribute(byte[] octets) {
+        super(CALLBACK_NUMBER);
+        str = new String(octets, 2, octets.length - 2);
+        this.octets = octets;
+    }
 
-	public String getString()
-	{
-		return _str;
-	}
+    /**
+     * Returns the callback number.
+     *
+     * @return the callback number
+     */
+    public String getString() {
+        return str;
+    }
 
-	public byte[] getValue() throws IOException
-	{
-		return _str.getBytes();
-	}
+    /**
+     * Returns the on-the-wire bytes used to construct this instance.
+     *
+     * @return the on-the-wire byte representation of this attribute.
+     */
+    public byte[] getValue() {
+        return octets;
+    }
+
+    /**
+     * Used by super class to log the attribute's contents when packet logging is enabled.
+     *
+     * @return content representation for traffic logging
+     */
+    public String toStringImpl() {
+        return str;
+    }
 }
