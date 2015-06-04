0d8260558522de0e0ee8c72d1f957ee3cf8a3965
Mark R. Boyd <boydmr@ldschurch.org>
2015-06-04 16:16:26 -0600

    checkstyle fixes

--- src/main/java/com/sun/identity/authentication/modules/radius/client/AttributeSet.java
+++ src/main/java/com/sun/identity/authentication/modules/radius/client/AttributeSet.java
@@ -32,45 +32,76 @@
 package com.sun.identity.authentication.modules.radius.client;
 
 
-import java.util.*;
+import java.util.Enumeration;
+import java.util.Vector;
 
-public class AttributeSet 
-{
-	private Vector _attrs = new Vector();
+/**
+ * Holder of the attribute instances in an instance of the {@link com.sun.identity.authentication.modules.radius.client
+ * .Packet} type which is the superclass of all radius packet types. Maintains the order in which attributes are added.
+ */
+public class AttributeSet {
+    /**
+     * Holder of the attribute instances.
+     */
+    private Vector attrs = new Vector();
 
-	public AttributeSet()
-	{
-	}
+    /**
+     * Constructor.
+     */
+    public AttributeSet() {
+    }
 
-	public void addAttribute(Attribute attr)
-	{
-		_attrs.addElement(attr);
-	}
+    /**
+     * Adds an attribute instance to the container.
+     *
+     * @param attr an attribute instance to be appended to the set
+     */
+    public void addAttribute(Attribute attr) {
+        attrs.addElement(attr);
+    }
 
-	public int size()
-	{
-		return _attrs.size();
-	}
+    /**
+     * Indicates the number of attributes held in the container.
+     *
+     * @return the number of contained attribute instances
+     */
+    public int size() {
+        return attrs.size();
+    }
 
-	public Enumeration getAttributes()
-	{
-		return _attrs.elements();
-	}
+    /**
+     * Returns a legacy {@link java.util.Enumeration} for enumerating over the the contained attribute instances.
+     *
+     * @return an {@link java.util.Enumeration} object for enumerating over the set of attribute instances.
+     */
+    public Enumeration getAttributes() {
+        return attrs.elements();
+    }
 
-	public Attribute getAttributeByType(int type)
-	{
-		int l = _attrs.size();
-		for (int i = 0; i < l; i++)
-		{
-			Attribute attr = getAttributeAt(i);
-			if (attr.getType() == type)
-				return attr;
-		}
-		return null;
-	}
+    /**
+     * Returns the first occurrence of an attribute instance of the specified type code.
+     *
+     * @param type the attribute type code to be searched for in the set of attributes in order of injection
+     * @return the first attribute instance incurred having the given type code or null if none is found.
+     */
+    public Attribute getAttributeByType(int type) {
+        int l = attrs.size();
+        for (int i = 0; i < l; i++) {
+            Attribute attr = getAttributeAt(i);
+            if (attr.getType() == type) {
+                return attr;
+            }
+        }
+        return null;
+    }
 
-	public Attribute getAttributeAt(int pos)
-	{
-		return (Attribute)_attrs.elementAt(pos);
-	}
+    /**
+     * Returns the attribute instance at the indicated index location.
+     *
+     * @param pos the position of the attribute instance to be retrieved.
+     * @return the instance of the attribute being retrieved
+     */
+    public Attribute getAttributeAt(int pos) {
+        return (Attribute) attrs.elementAt(pos);
+    }
 }
